b945c9a926875d36d243293a06a99ac6def46683
Pratik <pratikkurbet02@gmail.com>
2022-06-04 17:40:23 +0530

    2nd change

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
+  LoginForms:FormGroup
+
+  ngOnInit(): void
+  {
+    
   ngOnInit(): void {
   }
 
